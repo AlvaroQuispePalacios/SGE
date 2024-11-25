a61cd260b5c66385162c059429fdd0ce8e6ec456
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-11-25 21:14:55 +0100

    first commit

---
+++ Actividad_5.1/01_ejercicio.py
@@ -0,0 +1,9 @@
+x = input("Dime un numero: ")
+y = input("Dime otro numero: ")
+
+if( x > y):
+    print(f"El numero {x} es mayor que {y}")
+elif(x < y):
+    print(f"El numero {y} es mayor que {x}")
+else:
+    print("Los numeros son iguales")
---
+++ Actividad_5.1/02_ejercicio.py
@@ -0,0 +1,7 @@
+x = input("Dime un numero positivo mayor que 2: ")
+
+if(type(x) == int and x > 2):
+    print(f"El numero es {x}")
+    for i in range(3, x, 1):
+
+
