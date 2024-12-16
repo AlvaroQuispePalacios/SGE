72b772be044268d2ee537ae0664b77c30f222209
AlvaroQP <alonsol1569@gmail.com>
2024-12-16 23:33:02 +0100

    agenda casi hecha

--- Actividad_5.2/Agenda_Numeros_Correos.py
+++ Actividad_5.2/Agenda_Numeros_Correos.py
@@ -7,17 +7,84 @@ agenda = {
 seleccionar = -1
 
 while(seleccionar != 4):
-    seleccionar = int(input("1. Añadir contacto\n2. Modificar los numeros o correos electronicos de un contacto\n3. Buscar contactos por tipo de numero\n4. Salir"))
-
+    seleccionar = int(input("1. Añadir contacto\n2. Modificar los numeros o correos electronicos de un contacto\n3. Buscar contactos por tipo de numero\n4. Salir\n"))
+  
     if(seleccionar == 1):
+        nombre = ""
+        datos = []
+        correos = []
         nombreValido = False
+
         while(nombreValido == False):
             try:
-                nombre = str(input("Dime el nombre del estudiante: "))
+                nombre = str(input("Dime el nombre del contacto: "))
                 if(agenda.get(nombre) != None): 
                     raise ValueError
                 nombreValido = True
             except ValueError:
-                print("El alumno ya existe")
+                print("El contacto ya existe")
         
+
+        seleccionarAgregarNumero = -1
+        while(seleccionarAgregarNumero != 2):
+            seleccionarAgregarNumero = int(input("1. Agregar nuevo telefono\n2. Salir\n"))
+            if(seleccionarAgregarNumero == 1):
+                numero = int(input("Dime el numero de telefono: "))
+                categoria = ""
+                seleccionarCategoria = -1
+                while(seleccionarCategoria != 3):
+                    seleccionarCategoria = int(input("Seleccione la categoria:\n1. móvil\n2. trabajo\n3. Salir\n"))
+                    if(seleccionarCategoria == 1):
+                        categoria = "móvil"
+                    elif(seleccionarCategoria == 2):
+                        categoria = "trabajo"
+                    break
+                datos.append((numero, categoria))
+
         
+        seleccionarAgregarCorreo = -1
+        while(seleccionarAgregarCorreo != 2):
+            seleccionarAgregarCorreo = int(input("1. Agregar Correo\n2. Salir\n"))
+            if(seleccionarAgregarCorreo == 1):
+                correo = str(input("Dime el correo: "))
+                correos.append(correo)
+
+        agenda[nombre] = {
+            "datos": datos,
+            "correos": correos
+        }
+
+    elif(seleccionar == 2):
+        nombre = str(input("Dime el nombre del contacto: "))
+        contactoExiste = False
+        if(agenda.get(nombre) != None):
+            seleccionarModificar = -1
+            while(seleccionarModificar != 3):
+                seleccionarModificar = int(input("1. Modificar Correo\n2. Modificar numero telefono\n3. Salir\n"))
+                if(seleccionarModificar == 1):
+                    for numeros in agenda.get(nombre)["datos"]:
+                        
+
+        
+
+
+        
+
+        # for clave in agenda.keys():
+        #     if(clave == nombre):
+        #         contactoExiste = True
+        #         seleccionarModificar = -1
+        #         while(seleccionarModificar != 3):
+        #             seleccionarModificar = int(input("1. Modificar Correo\n2. Modificar numero telefono"))
+
+        #         break
+
+
+            
+     
+
+
+
+print(agenda)
+
+
